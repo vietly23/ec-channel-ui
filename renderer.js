8b525c27bee1a256e014bc047a69f2217052060f
sykuo <sykuo@uci.edu>
2016-08-31 11:17:32 -0700

    Bar graph on home screen changed to donut graph
    donut graph reads data from mydata.json as a local file

--- renderer.js
+++ renderer.js
@@ -4,7 +4,7 @@
 var d3  = require("d3");
 var data = require('./mydata.json');
 
-var canvas = d3.select("body").append("svg")
+/*var canvas = d3.select("body").append("svg")
   .attr("width", 500)
   .attr("height", 500)
 
@@ -15,7 +15,34 @@ canvas.selectAll("rect")
   .attr("width", function(d){return d.count*10;})
   .attr("height",48)
   .attr("y", function(d, i) {return i*50;})
-  .attr("fill", "blue")
+  .attr("fill", "blue")*/
+
+  var width = 500;
+  var height = 500;
+  var radius = Math.min(width, height) / 2;
+  var donutWidth = 75;                            // NEW
+  var color = d3.scaleOrdinal(d3.schemeCategory20b);
+  var svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2) +
+      ',' + (height / 2) + ')');
+  var arc = d3.arc()
+    .innerRadius(radius - donutWidth)             // UPDATED
+    .outerRadius(radius);
+  var pie = d3.pie()
+    .value(function(d) { return d.count; })
+    .sort(null);
+  var path = svg.selectAll('path')
+    .data(pie(data))
+    .enter()
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', function(d, i) {
+      return color(d.data.label);
+    });
 
 
 
