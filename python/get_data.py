a1086f4f4954c47c0399a287130bf13c014ee350
SpicyMeatball <vietly23@gmail.com>
2016-09-27 11:43:31 -0700

    Placeholder for real-time data

--- python/get_data.py
+++ python/get_data.py
@@ -1,4 +1,16 @@
 import random
+import json
 
 if __name__ == '__main__':
-	print('{:.2f}'.format(5 * random.random() + 12))
+	current_demand = round(random.random() * 5, 2) + 9
+	air_conditioning = round(random.random() * 10, 2) + 20
+	lighting =  round(random.random() * 5, 2) + 15
+	home_appliance = round(random.random() * 7, 2) + 48;
+	misc = round(random.random() * 4, 2) + 5;
+	power_consumption = {'current_demand' : current_demand, 
+			'air_conditioning' : air_conditioning, 
+			'lighting' : lighting,
+			'home_appliances' : home_appliance,
+			'misc' : misc}
+	print(json.dumps(power_consumption))
+	
